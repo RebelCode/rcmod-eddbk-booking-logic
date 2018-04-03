cd887de6f7c6457eef8f3d995d1b8c95c994f3e4
Miguel Muscat <miguelmuscat93@gmail.com>
2018-04-03 16:26:16 +0200

    Added module main file

---
+++ module.php
@@ -0,0 +1,17 @@
+<?php
+
+use Psr\Container\ContainerInterface;
+use RebelCode\EddBookings\Logic\Module\EddBookingsLogicModule;
+
+define('EDDBK_BOOKING_LOGIC_MODULE_DIR', __DIR__);
+define('EDDBK_BOOKING_LOGIC_MODULE_CONFIG', __DIR__ . DIRECTORY_SEPARATOR . 'config.php');
+define('EDDBK_BOOKING_LOGIC_MODULE_KEY', 'eddbk_booking_logic');
+
+return function(ContainerInterface $c) {
+    return new EddBookingLogicModule(
+        EDDBK_BOOKING_LOGIC_MODULE_KEY,
+        $c->get('container_factory'),
+        $c->get('event_manager'),
+        $c->get('event_factory')
+    );
+};
