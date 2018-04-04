77c1398b10d280f2009d543633464168ea6214b9
Miguel Muscat <miguelmuscat93@gmail.com>
2018-04-04 18:17:42 +0200

    Added new booking transitioner service

--- src/EddBookingsLogicModule.php
+++ src/EddBookingsLogicModule.php
@@ -8,6 +8,7 @@ use Dhii\Exception\InternalException;
 use Dhii\Util\String\StringableInterface as Stringable;
 use Psr\Container\ContainerInterface;
 use Psr\EventManager\EventManagerInterface;
+use RebelCode\Bookings\FactoryStateMachineTransitioner;
 use RebelCode\Modular\Module\AbstractBaseModule;
 
 /**
@@ -51,6 +52,16 @@ class EddBookingsLogicModule extends AbstractBaseModule
 
         return $this->_createContainer(
             [
+                'booking_transitioner'           => function(ContainerInterface $c) {
+                    return new EventsDelegateTransitioner(
+                        new FactoryStateMachineTransitioner(
+                            $c->get('booking_state_machine_provider'),
+                            $c->get('booking_factory')
+                        ),
+                        $c->get('event_manager'),
+                        $c->get('event_factory')
+                    );
+                },
                 'booking_state_machine_provider' => function(ContainerInterface $c) use ($config) {
                     return new BookingStateMachineProvider($c, $config);
                 },
