4021f146369090a5c6d34286b4340569fc8215ba
Miguel Muscat <miguelmuscat93@gmail.com>
2018-04-04 18:13:52 +0200

    Added `EventsDelegateTransitioner`

---
+++ src/EventsDelegateTransitioner.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace RebelCode\EddBookings\Logic\Module;
+
+use Dhii\Event\EventFactoryInterface;
+use Psr\EventManager\EventManagerInterface;
+use RebelCode\Bookings\BookingInterface;
+use RebelCode\Bookings\TransitionerAwareTrait;
+use RebelCode\Bookings\TransitionerInterface;
+use RebelCode\Modular\Events\EventsConsumerTrait;
+
+/**
+ * A booking transitioner that triggers events before and after a transition, while delegating transitioning to another
+ * transitioner instance.
+ *
+ * @since [*next-version*]
+ */
+class EventsDelegateTransitioner implements TransitionerInterface
+{
+    /*
+     * Provides awareness of a booking transitioner.
+     *
+     * @since [*next-version*]
+     */
+    use TransitionerAwareTrait;
+
+    /*
+     * Provides all required functionality for working with events.
+     *
+     * @since [*next-version*]
+     */
+    use EventsConsumerTrait;
+
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param TransitionerInterface      $transitioner The booking transition.
+     * @param EventManagerInterface|null $eventManager The event manager, if any.
+     * @param EventFactoryInterface|null $eventFactory The event factory, if any.
+     */
+    public function __construct(
+        TransitionerInterface $transitioner,
+        $eventManager = null,
+        $eventFactory = null
+    ) {
+        $this->_setTransitioner($transitioner);
+        $this->_setEventManager($eventManager);
+        $this->_setEventFactory($eventFactory);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function transition(BookingInterface $booking, $transition)
+    {
+        // Trigger an event before transitioning, filtering the booking
+        $params = ['booking' => $booking, 'transition' => $transition];
+        $booking = $this->_trigger('before_booking_transition', $params)->getParam('booking');
+
+        // Transition the booking
+        $booking = $this->_getTransitioner()->transition($booking, $transition);
+
+        // Trigger an event after transitioning, filtering the booking
+        $params = ['booking' => $booking, 'transition' => $transition];
+        $booking = $this->_trigger('after_booking_transition', $params)->getParam('booking');
+
+        return $booking;
+    }
+}
