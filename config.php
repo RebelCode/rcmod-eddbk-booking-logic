47567e79846833600b1cfc8b2c1707c72f688bb8
Miguel Muscat <miguelmuscat93@gmail.com>
2018-04-18 15:37:36 +0200

    Updated config

--- config.php
+++ config.php
@@ -4,49 +4,49 @@ use RebelCode\EddBookings\Logic\Module\BookingStatusInterface as S;
 use RebelCode\EddBookings\Logic\Module\BookingTransitionInterface as T;
 
 return [
-    'booking_event_state_machine' => [
-        'event_name_format' => 'on_booking_transition',
-    ],
-    'booking_status_transitions'  => [
-        S::STATUS_NONE      => [
-            T::TRANSITION_CART  => S::STATUS_IN_CART,
-            T::TRANSITION_DRAFT => S::STATUS_DRAFT,
-        ],
-        S::STATUS_IN_CART   => [
-            T::TRANSITION_SUBMIT => S::STATUS_PENDING,
-        ],
-        S::STATUS_DRAFT     => [
-            T::TRANSITION_SUBMIT => S::STATUS_PENDING,
-            T::TRANSITION_DRAFT  => S::STATUS_DRAFT,
-        ],
-        S::STATUS_PENDING   => [
-            T::TRANSITION_APPROVE => S::STATUS_APPROVED,
-            T::TRANSITION_REJECT  => S::STATUS_REJECTED,
-        ],
-        S::STATUS_APPROVED  => [
-            T::TRANSITION_SCHEDULE => S::STATUS_SCHEDULED,
-            T::TRANSITION_CANCEL   => S::STATUS_CANCELLED,
-        ],
-        S::STATUS_REJECTED  => [
-            T::TRANSITION_SUBMIT => S::STATUS_PENDING,
-        ],
-        S::STATUS_SCHEDULED => [
-            T::TRANSITION_COMPLETE => S::STATUS_COMPLETED,
-            T::TRANSITION_CANCEL   => S::STATUS_CANCELLED,
-        ],
-        S::STATUS_COMPLETED => [
-            T::TRANSITION_DRAFT    => S::STATUS_DRAFT,
-            T::TRANSITION_SUBMIT   => S::STATUS_PENDING,
-            T::TRANSITION_APPROVE  => S::STATUS_APPROVED,
-            T::TRANSITION_SCHEDULE => S::STATUS_SCHEDULED,
-            T::TRANSITION_CANCEL   => S::STATUS_CANCELLED,
-        ],
-        S::STATUS_CANCELLED => [
-            T::TRANSITION_DRAFT    => S::STATUS_DRAFT,
-            T::TRANSITION_SUBMIT   => S::STATUS_PENDING,
-            T::TRANSITION_APPROVE  => S::STATUS_APPROVED,
-            T::TRANSITION_SCHEDULE => S::STATUS_SCHEDULED,
-            T::TRANSITION_COMPLETE => S::STATUS_COMPLETED,
+    'booking_logic' => [
+        'transition_event_format' => 'on_booking_transition',
+        'status_transitions'      => [
+            S::STATUS_NONE      => [
+                T::TRANSITION_CART  => S::STATUS_IN_CART,
+                T::TRANSITION_DRAFT => S::STATUS_DRAFT,
+            ],
+            S::STATUS_IN_CART   => [
+                T::TRANSITION_SUBMIT => S::STATUS_PENDING,
+            ],
+            S::STATUS_DRAFT     => [
+                T::TRANSITION_SUBMIT => S::STATUS_PENDING,
+                T::TRANSITION_DRAFT  => S::STATUS_DRAFT,
+            ],
+            S::STATUS_PENDING   => [
+                T::TRANSITION_APPROVE => S::STATUS_APPROVED,
+                T::TRANSITION_REJECT  => S::STATUS_REJECTED,
+            ],
+            S::STATUS_APPROVED  => [
+                T::TRANSITION_SCHEDULE => S::STATUS_SCHEDULED,
+                T::TRANSITION_CANCEL   => S::STATUS_CANCELLED,
+            ],
+            S::STATUS_REJECTED  => [
+                T::TRANSITION_SUBMIT => S::STATUS_PENDING,
+            ],
+            S::STATUS_SCHEDULED => [
+                T::TRANSITION_COMPLETE => S::STATUS_COMPLETED,
+                T::TRANSITION_CANCEL   => S::STATUS_CANCELLED,
+            ],
+            S::STATUS_COMPLETED => [
+                T::TRANSITION_DRAFT    => S::STATUS_DRAFT,
+                T::TRANSITION_SUBMIT   => S::STATUS_PENDING,
+                T::TRANSITION_APPROVE  => S::STATUS_APPROVED,
+                T::TRANSITION_SCHEDULE => S::STATUS_SCHEDULED,
+                T::TRANSITION_CANCEL   => S::STATUS_CANCELLED,
+            ],
+            S::STATUS_CANCELLED => [
+                T::TRANSITION_DRAFT    => S::STATUS_DRAFT,
+                T::TRANSITION_SUBMIT   => S::STATUS_PENDING,
+                T::TRANSITION_APPROVE  => S::STATUS_APPROVED,
+                T::TRANSITION_SCHEDULE => S::STATUS_SCHEDULED,
+                T::TRANSITION_COMPLETE => S::STATUS_COMPLETED,
+            ],
         ],
     ],
 ];
